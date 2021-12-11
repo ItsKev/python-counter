2e6a0cedf87cf9c52dc8333d2c9e04410aeef56f
Kevin Huber <kevin.huber@hotmail.com>
2021-12-11 07:17:43 +0100

    Add linkerd to the redis namespace

--- infrastructure/setup.sh
+++ infrastructure/setup.sh
@@ -6,9 +6,10 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-helm install redis --namespace redis --create-namespace bitnami/redis --values configs/redis-values.yaml
-
 linkerd install | kubectl apply -f -
 linkerd viz install | kubectl apply -f -
+
+kubectl create -f k8s/redis-namespace.yaml
+helm install redis --namespace redis bitnami/redis --values configs/redis-values.yaml
 
 helm install prometheus-adapter prometheus-community/prometheus-adapter --namespace linkerd-viz --values configs/prometheus-adapter.values.yaml
